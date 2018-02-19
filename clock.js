970560819eb9076185563aaa908c06d051ec5da1
Sophie <sophie@Sophies-MacBook-Air.local>
2018-02-18 18:27:48 -0800

    Initial commit

---
+++ clock.js
@@ -0,0 +1,227 @@
+/** 
+ * function to get a Date object given a specific
+ * UTC offset
+ */
+function newDateWithTimezone(offset) {
+    // create Date object for current location
+    var d = new Date();
+
+    // convert to msec
+    // add local time zone offset
+    // get UTC time in msec
+    var utc = d.getTime() + (d.getTimezoneOffset() * 60000);
+
+    // create new Date object for different city
+    // using supplied offset
+    return new Date(utc + (3600000*offset));
+}
+
+var usr_color = 160; //Change value to change color scheme
+
+window.requestAnimFrame = (function(){
+  return  window.requestAnimationFrame || 
+    window.webkitRequestAnimationFrame || 
+    window.mozRequestAnimationFrame    || 
+    window.oRequestAnimationFrame      || 
+    window.msRequestAnimationFrame     ||  
+    function( callback ){
+    window.setTimeout(callback, 1000 / 60);
+  };
+})();
+
+var canvas = document.getElementsByTagName("canvas")[0];
+var ctx = canvas.getContext("2d");
+
+var w = 600, h = 600;
+canvas.width = w;
+canvas.height = h;
+
+var arcs = [];
+
+function init(){
+  console.log("init called");
+  reset();
+  arcs = [];
+  var m = new arc();
+  m.class = "month";
+  arcs.push(m);
+  
+  var d = new arc();
+  d.class = "date";
+  d.r = 135;
+  arcs.push(d);
+  
+  var d = new arc();
+  d.class = "day";
+  d.r = 170;
+  arcs.push(d);
+  
+  var h = new arc();
+  h.class = "hours";
+  h.r = 205;
+  arcs.push(h);
+  
+  var m = new arc();
+  m.class = "mins";
+  m.r = 240;
+  arcs.push(m);
+  
+  var s = new arc();
+  s.class = "secs";
+  s.r = 275;
+  arcs.push(s);
+}
+
+function arc(){
+  this.class = "month";
+  this.r = 100;
+  this.rot = 1;
+  
+  this.draw = function(){
+    ctx.beginPath();
+    ctx.arc(300,300,this.r,(Math.PI/(2/3)),this.rot,false);
+    ctx.lineWidth = 30;
+    ctx.strokeStyle = "hsla("+(this.rot*(180/Math.PI)+usr_color)+",60%,50%,1)";
+    ctx.stroke();
+    
+    ctx.save();
+    ctx.fillStyle = "#333";
+    ctx.translate(300, 300);
+    ctx.rotate(this.rot);
+    ctx.font="14px Arial Rounded MT Bold";
+    if(this.class == "secs"){
+      var d = newDateWithTimezone(+11);
+      ctx.fillText(d.getSeconds(), 267, -5);
+    }
+    else if(this.class == "mins"){
+      var d = newDateWithTimezone(+11);
+      ctx.fillText(d.getMinutes(), 233, -5);
+    }
+    else if(this.class == "hours"){
+      var d = newDateWithTimezone(+11);
+      ctx.fillText(d.getHours(), 197, -5);
+    }
+    else if(this.class == "day"){
+      var d = newDateWithTimezone(+11);
+      var day = d.getDay();
+      if(day == 0){
+        var day = "Sun"
+      }
+      else if(day == 1){
+        var day = "Mon"
+      }
+      else if(day == 2){
+        var day = "Tue"
+      }
+      else if(day == 3){
+        var day = "Wed"
+      }
+      else if(day == 4){
+        var day = "Thu"
+      }
+      else if(day == 5){
+        var day = "Fri"
+      }
+      else if(day == 6){
+        var day = "Sat"
+      }
+      ctx.fillText(day, 158, -5);
+    }
+    else if(this.class == "date"){
+      var d = newDateWithTimezone(+11);
+      ctx.fillText(d.getDate(), 127, -5);
+    }
+    else if(this.class == "month"){
+      var d = newDateWithTimezone(+11);
+      ctx.fillText(d.getMonth()+1, 97, -5);
+    }
+    ctx.restore();
+  }
+}
+
+function reset(){
+  ctx.fillStyle = "#333";
+  ctx.fillRect(0,0,w,h);
+}
+
+function draw(){
+  reset();
+  ctx.fillStyle = "rgba(255,255,255,0.5)";
+  ctx.font = "12px Arial"
+  ctx.fillText("seconds", 250, 27);
+  ctx.fillText("minutes", 252, 63);
+  ctx.fillText("hours", 264, 98);
+  ctx.fillText("day", 274, 134);
+  ctx.fillText("date", 270, 168);
+  ctx.fillText("month", 260, 203);
+  
+  for(var i=0;i<arcs.length;i++){
+    var a = arcs[i];
+    var d = new Date();
+    if(a.class == "month"){
+      var n = d.getMonth()+1;
+      a.rot = (n/12)*(Math.PI*2) - (Math.PI/2);
+    }
+    else if(a.class == "date"){
+      var n = d.getDate();
+      a.rot = (n/31)*(Math.PI*2) - (Math.PI/2);
+    }
+    else if(a.class == "day"){
+      var n = d.getDay();
+      a.rot = (n/7)*(Math.PI*2) - (Math.PI/2);
+    }
+    else if(a.class == "hours"){
+      var n = d.getHours();
+      var m = d.getMinutes();
+      a.rot = ((n/12)*(Math.PI*2) - (Math.PI/2)) + ((m/3600)*(Math.PI*2));
+    }
+    else if(a.class == "mins"){
+      var n = d.getMinutes();
+      var s = d.getSeconds();
+      a.rot = ((n/60)*(Math.PI*2) - (Math.PI/2)) + ((s/3600)*(Math.PI*2));
+    }
+    else if(a.class == "secs"){
+      var n = d.getSeconds();
+      var m = d.getMilliseconds();
+      a.rot = ((n/60)*(Math.PI*2) - (Math.PI/2)) + ((m/60000)*(Math.PI*2));
+    }
+    a.draw();
+  }
+}
+
+function animloop() {
+  draw();
+  requestAnimFrame(animloop); 
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+  init();
+  animloop();
+}, false);
+
+
+function clock() {// We create a new Date object and assign it to a variable called "time".
+var time = new Date(),
+    
+    // Access the "getHours" method on the Date object with the dot accessor.
+    hours = time.getHours(),
+    
+    // Access the "getMinutes" method with the dot accessor.
+    minutes = time.getMinutes(),
+    
+    
+    seconds = time.getSeconds();
+
+document.querySelectorAll('.clock')[0].innerHTML = harold(hours) + ":" + harold(minutes) + ":" + harold(seconds) + " PST";
+  
+  function harold(standIn) {
+    if (standIn < 10) {
+      standIn = '0' + standIn
+    }
+    return standIn;
+  }
+}
+
+clock();
+
+setInterval(clock, 1000);
---
+++ q1project.js
@@ -0,0 +1,175 @@
+function weightConverter(valNum) {
+  document.getElementById("outputKilograms").innerHTML = valNum / 2.2046;
+}
+
+function distanceConverter(valNum) {
+  document.getElementById("outputKilometres").innerHTML = valNum / 0.62137;
+}
+
+function volumeConverter(valNum) {
+  document.getElementById("outputLitres").innerHTML = valNum / 0.264172037;
+}
+
+function smallweightConverter(valNum) {
+  document.getElementById("outputGrams").innerHTML = valNum / 0.03527399;
+}
+
+function lengthConverter() {
+  var feet = parseFloat(document.getElementById('inputFeet').value || "0");
+  var inches = parseFloat(document.getElementById('inputInches').value || "0");
+  document.getElementById("outputMetres").innerHTML = (feet + (inches/12.0)) / 3.28084;
+}
+
+var forexData;
+$.get("https://api.fixer.io/latest", function(data) {
+  forexData = data;
+  console.log(forexData);
+});
+
+function AUDConverter(valNum) {
+  var conversionFactor = forexData.rates.AUD / forexData.rates.USD;
+  document.getElementById("outputUSD").innerHTML = valNum / conversionFactor;
+}
+
+function USDConverter(valNum) {
+  var conversionFactor = forexData.rates.USD / forexData.rates.AUD;
+  document.getElementById("outputAUD").innerHTML = valNum / conversionFactor;
+}
+
+$(document).ready(function() {
+  let favorites = JSON.parse(localStorage.getItem("favorites"));
+  if (!favorites) {
+    favorites = {};
+  }
+
+  const renderTable = function(slang) {
+    const $rootDiv = $('#translations-table');
+
+    $rootDiv.html('');
+
+    for (const entry of slang) {
+      const $divAustralian = $('<div class="col-4">').text(entry.australian);
+      const $divAmerican = $('<div class="col-6">').text(entry.american);
+
+      const $divFav = $('<div class="col-2">').css('cursor', 'ew-resize');
+
+      if (favorites[entry.id]) {
+        $divFav.text('❤️');
+      }
+      else {
+        $divFav.text('💔');
+      }
+
+      $divFav.on('click', () => {
+        favorites[entry.id] = !favorites[entry.id];
+        localStorage.setItem("favorites", JSON.stringify(favorites));
+        renderTable(slang);
+      });
+
+      const $divRow = $('<div class="row">');
+
+      $divRow.append($divAustralian);
+      $divRow.append($divAmerican);
+      $divRow.append($divFav);
+
+      $rootDiv.append($divRow);
+    }
+  };
+
+  var slang = [
+    {
+      id: 0,
+      australian: 'Car Park',
+      american: 'Parking Lot'
+    },
+    {
+      id: 1,
+      australian: 'Heaps',
+      american: 'A Lot'
+    },
+    {
+      id: 2,
+      australian: 'Mates',
+      american: 'Friends'
+    },
+    {
+      id: 3,
+      australian: 'Bogans',
+      american: 'Rednecks'
+    },
+    {
+      id: 4,
+      australian: 'Mozzies',
+      american: 'Mosquitoes'
+    },
+    {
+      id: 5,
+      australian: 'Petro',
+      american: 'Gas'
+    },
+    {
+      id: 6,
+      australian: 'No Drama',
+      american: 'No Problem'
+    },
+    {
+      id: 7,
+      australian: 'Thong',
+      american: 'Flip-flops'
+    },
+    {
+      id: 8,
+      australian: 'Wanker',
+      american: 'Annoying Individual'
+    },
+    {
+      id: 9,
+      australian: 'Feral',
+      american: 'Messy/Untamed'
+    },
+    {
+      id: 10,
+      australian: 'Keen',
+      american: 'Want to do something'
+    },
+    {
+      id: 11,
+      australian: 'Reckon',
+      american: 'Recommend/Consider'
+    },
+    {
+      id: 12,
+      australian: 'Thunder',
+      american: 'Fart'
+    },
+    {
+      id: 13,
+      australian: 'Play the flute',
+      american: 'Masturbate'
+    },
+    {
+      id: 14,
+      australian: 'Knackered',
+      american: 'Exhausted'
+    },
+    {
+      id: 15,
+      australian: 'Knackers',
+      american: 'Balls/Testicles'
+    },
+    {
+      id: 16,
+      australian: 'Barbie',
+      american: 'Barbecue'
+    },
+    {
+      id: 17,
+      australian: 'Servo',
+      american: 'Service Station'
+    }
+  ];
+
+  renderTable(slang);
+});
+
+
