4277ce39f4938abad77efed6fa518bb743828ad4
Sophie <sophie@Sophies-MacBook-Air.local>
2018-02-26 14:57:32 -0800

    delete pictures and added a few comments

--- clock.js
+++ clock.js
@@ -9,10 +9,10 @@ function newDateWithTimezone(offset) {
     // convert to msec
     // add local time zone offset
     // get UTC time in msec
-    var utc = d.getTime() + (d.getTimezoneOffset() * 60000);
+    var utc = d.getTime() + (d.getTimezoneOffset() * 60000); 
+    //getTimezoneOffset get time zone difference in minutes
 
-    // create new Date object for different city
-    // using supplied offset
+   
     return new Date(utc + (3600000*offset));
 }
 
